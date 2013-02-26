673132a3586c014bef842152ac2d831c0151fd1b
Christoph Hütt <christoph.huett@gmail.com>
2013-02-26 13:11:35 +0100

    cleanup created more meaningful files and install scripts

---
+++ installapachephp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Apache:
+#Infos: http://wiki.ubuntuusers.de/Apache
+#Install: 
+sudo apt-get install apache2 apache2-doc
+#Test: localhost
+
+#PHP
+#Info: http://wiki.ubuntuusers.de/PHP
+#Install: 
+sudo apt-get install php5 
+#Testen:
+#cd /var/www
+#sudo gedit phpinfo.php
+#einfügen und speichern: <?php phpinfo();?>
+#Testen: localhost/phpinfo.php
